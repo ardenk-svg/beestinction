067ce5eaefc31394a87831facfe9393eb94fcc6d
Arden Knee <ardenk@umich.edu>
2026-09-19 18:55:20 -0400

    Initial commit

---
+++ bees.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <cctype>
+#include <ranges>
+#include <cstdlib>
+#include <sstream>
+#include <ctime>
+using namespace std;
+
+void bee() {
+    int bee = 1;
+    cout << bee << endl;
+}
